bdb2b198286c84efca0da8d297a11c007f16938b
kerolosghatas255-netizen <kerolosghatas255@gmail.com>
2026-09-25 20:39:07 +0300

    infra: bring existing AWS resources under Terraform

---
+++ infra/terraform/main.tf
@@ -0,0 +1,74 @@
+resource "aws_security_group" "web" {
+  name        = "aws-docker-webapp-sg"
+  description = "Security group for AWS Docker portfolio project"
+  vpc_id      = "vpc-067e7ebd6216d9464"
+
+  ingress {
+    description = "SSH from trusted IP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.ssh_allowed_cidr]
+  }
+
+  ingress {
+    description = "Public HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+resource "aws_instance" "web" {
+  ami           = "ami-05bfa4a7765f38076"
+  instance_type = "t3.micro"
+
+  subnet_id                   = "subnet-0d8a35d174a189f1e"
+  associate_public_ip_address = true
+
+  key_name             = "aws-docker-webapp-key"
+  iam_instance_profile = "aws-docker-webapp-ssm-role"
+
+  vpc_security_group_ids = [
+    aws_security_group.web.id
+  ]
+
+  metadata_options {
+    http_endpoint               = "enabled"
+    http_tokens                 = "required"
+    http_put_response_hop_limit = 2
+    http_protocol_ipv6          = "disabled"
+    instance_metadata_tags      = "disabled"
+  }
+
+  root_block_device {
+    volume_type           = "gp3"
+    volume_size           = 10
+    iops                  = 3000
+    throughput            = 125
+    encrypted             = true
+    kms_key_id            = "arn:aws:kms:eu-north-1:563807597162:key/4a57c50c-8754-4a53-b4b8-0cc766f6f722"
+    delete_on_termination = true
+  }
+
+  credit_specification {
+    cpu_credits = "unlimited"
+  }
+
+  tags = {
+    Name = "aws-docker-webapp"
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
---
+++ infra/terraform/variables.tf
@@ -0,0 +1,4 @@
+variable "ssh_allowed_cidr" {
+  description = "CIDR allowed to connect to the EC2 instance over SSH"
+  type        = string
+}
